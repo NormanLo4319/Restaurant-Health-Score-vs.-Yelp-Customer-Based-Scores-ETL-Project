065d9e56c0606ccb01c6aedb64e676b2b5cb164a
NormanLo4319 <lokman@mail.sfsu.edu>
2019-05-16 17:05:18 -0700

    ETL Project Completed

---
+++ ETL_Project_SQL_Quary.sql
@@ -0,0 +1,20 @@
+''' create database ETL_Project_db; '''
+
+use ETL_Project_db;
+
+SELECT inspection.name 
+FROM inspection
+WHERE inspection_score = 100;
+
+SELECT yelp.name
+FROM yelp
+WHERE rating = 5;
+
+SELECT risk_category, COUNT(name) as 'Total Count'
+FROM inspection
+GROUP BY risk_category
+
+SELECT inspection.name, inspeciont.inspection_score, yelp.rating, yelp.reviews
+FROM inspection
+INNER JOIN yelp
+ON inspection.name = yelp.name;
---
+++ config_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Yelp Key
+ID = "kZfKwbVYK_4yJHOY3mL6gQ"
+ykey = "obxkOqO4VMp8qneeDjYdwoNgO0rNCWR9tf1RUdf51kmzxfV8zaXrRDBDXfWSH7_DzPMy3kU1BsVBTwx-MzscwYzshsHphAsxaiqd10OPpAMlefPRoJbM6bx9NfaaXHYx"
+
